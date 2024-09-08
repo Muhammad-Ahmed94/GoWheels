b257048b996e514bffb6d84c6fad8db80d964d22
Muneeb Ahmed94 <muneebbum757@gmail.com>
2024-09-08 15:06:32 +0500

    Header complete.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import CustomButton from "./CustomButton";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <header className="w-full flex absolute z-10 mx-auto">
+      <nav className="pt-6 px-12 w-full sm:px-7 flex justify-between items-center">
+        <Link href="/" className="flex justify-center items-center">
+          <Image src="/logo.jpg" alt="logo" width={90} height={90} />{" "}
+          <span className="ml-1 font-bold">GoWheels</span>
+        </Link>
 
-export default Navbar
+        <CustomButton
+          title="Sign In"
+          containerStyles="bg-blue-400 text-white font-bold rounded"
+        />
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
