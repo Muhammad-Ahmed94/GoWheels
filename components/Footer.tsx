3d4346101d3c2e17e945b706be10a3ac957e43a4
Muneeb Ahmed94 <muneebbum757@gmail.com>
2024-09-08 16:50:05 +0500

    Footer complete. Add constants file&data.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+import { footerLinks } from '@/constants';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="flex flex-col flex-wrap px-12 border-t border-gray-300">
+      <div className="flex max-md:flex-col flex-wrap justify-between gap-5 my-5">
+        <div className="flex flex-col gap-1">
+          <Image src="/logo.webp" alt="logo" width={90} height={90} />
+          <p>@GoWheels 2024</p>
+          <p>&copy;All rights reserved</p>
+        </div>
 
-export default Footer
+        <div className="flex flex-wrap gap-20 justify-end">
+          {footerLinks.map((footerLink) => (
+            <div key={footerLink.title}>
+              <h3 className="font-bold mb-2">{footerLink.title}</h3>
+              <div>
+                {footerLink.links.map((link) => (
+                  <Link
+                    key={link.title}
+                    href={link.url}
+                    className="flex flex-col text-gray-500 border-b-2 border-dotted"
+                  >
+                    {link.title}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex justify-center items-center gap-4 pb-4 sm:flex-nowrap">
+        <p>@2024 GoWheels. All rights reserved.</p>
+        <Link href="/">Privacy Policy</Link>
+        <Link href="/">Terms of Use</Link>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ constants/index.ts
@@ -0,0 +1,29 @@
+export const footerLinks = [
+  {
+    title: "About",
+    links: [
+      { title: "How it wokrs", url: "/" },
+      { title: "Featured", url: "/" },
+      { title: "Partnership", url: "/" },
+      { title: "Business Relations", url: "/" },
+    ],
+  },
+  {
+    title: "Company",
+    links: [
+      { title: "Events", url: "/" },
+      { title: "Blogs", url: "/" },
+      { title: "Podcasts", url: "/" },
+      { title: "Invite others", url: "/" },
+    ],
+  },
+  {
+    title: "Socials",
+    links: [
+      { title: "Facebook", url: "https://facebook.com" },
+      { title: "Instagram", url: "https://instagram.com" },
+      { title: "Twitter", url: "https://x.com/home" },
+      { title: "Discord", url: "https://discord.com" },
+    ],
+  },
+];
